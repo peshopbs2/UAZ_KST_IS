d9011a5a630579e4739e323c578ef85b18d63639
peshopbs2 <peshopbs2@gmail.com>
2025-09-17 06:22:30 -0800

    add menu view models

---
+++ UAZ_KST_IS.Models/Domain/ViewModels/Menu/MenuEditViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAZ_KST_IS.Models.Domain.ViewModels.Menu
+{
+    public class MenuEditViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+    }
+}
