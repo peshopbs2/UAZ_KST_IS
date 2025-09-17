d0fb42d7cdeb35b8632de2abd006dd16f20f1f01
peshopbs2 <peshopbs2@gmail.com>
2025-09-17 04:56:24 -0800

    add role names constants

---
+++ UAZ_KST_IS.Models/Domain/Constants/RoleNames.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAZ_KST_IS.Models.Domain.Constants
+{
+    public static class RoleNames
+    {
+        public const string Administrator = "Administrator";
+        public const string Guest = "Guest";
+    }
+}
